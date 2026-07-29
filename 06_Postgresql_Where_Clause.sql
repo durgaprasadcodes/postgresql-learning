12c5b5a11f664260edbbd23bb60b313dd1f409bd
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-07-29 16:23:05 +0530

    PostgreSQL Where Clause

---
+++ 06_Postgresql_Where_Clause.sql
@@ -0,0 +1,33 @@
+
+CREATE TABLE IF NOT EXISTS USERS(
+    ID SERIAL PRIMARY KEY,
+    NAME VARCHAR(50) NOT NULL,
+    AGE INTEGER ,
+    EMAIL VARCHAR(50) NOT NULL,
+    CREATION_TIME TIMESTAMP DEFAULT NOW(),
+    CREATION_DATE DATE DEFAULT CURRENT_DATE
+);
+
+
+INSERT INTO USERS(name,age,email) VALUES
+    ('Durga Prasad Kota',20,'durgaprasad04289@gmail.com'),
+    ('Sidharadha Thutika',18,'siddardhathutika@gmail.com'),
+    ('Jayanth Macharla',21,'jayanth@gmail.com'),
+    ('Ganesh Gonela',20,'ganeshgonela@gmail.com'),
+    ('Pavan Kumar Mandalapalli',20,'pavankumar@gmail.com'),
+    ('Vijay Kumar Konatham',22,'vijaykumar@gmail.com'),
+    ('Sudheer Kodelli',25,'sudheerkodelli@gmail.com'),
+    ('Varun Chede',50,'varunchede@gmail.com');
+
+ALTER TABLE USERS
+ADD VILLAGE VARCHAR(50) ;
+
+SELECT * FROM USERS WHERE AGE>25;
+
+SELECT NAME,AGE,EMAIL FROM USERS WHERE NAME = 'Varun Chede';
+
+SELECT SUM(AGE) FROM USERS WHERE AGE<20 AND AGE>15;
+
+SELECT EMAIL FROM USERS  WHERE EMAIL LIKE '%gmail.com';
+
+SELECT EMAIL FROM USERS WHERE NAME ~ 'rga';
