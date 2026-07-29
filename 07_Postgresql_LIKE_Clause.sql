8cb2f1437749105f9553c624a775b2ba1490704b
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-07-29 16:35:32 +0530

    PostgreSQL LIKE Clause

--- 07_Postgresql_LIKE_Clause.sql
+++ 07_Postgresql_LIKE_Clause.sql
@@ -1,22 +1,22 @@
 
--- CREATE TABLE IF NOT EXISTS USERS(
---     ID SERIAL PRIMARY KEY,
---     NAME VARCHAR(50) NOT NULL,
---     AGE INTEGER ,
---     EMAIL VARCHAR(50) NOT NULL UNIQUE,
---     CREATION_DATE DATE DEFAULT CURRENT_DATE
--- );
+CREATE TABLE IF NOT EXISTS USERS(
+    ID SERIAL PRIMARY KEY,
+    NAME VARCHAR(50) NOT NULL,
+    AGE INTEGER ,
+    EMAIL VARCHAR(50) NOT NULL UNIQUE,
+    CREATION_DATE DATE DEFAULT CURRENT_DATE
+);
 
 
--- INSERT INTO USERS(name,age,email) VALUES
---     ('Durga Prasad Kota',20,'durgaprasad04289@gmail.com'),
---     ('Sidharadha Thutika',18,'siddardhathutika@gmail.com'),
---     ('Jayanth Macharla',21,'jayanth@gmail.com'),
---     ('Ganesh Gonela',20,'ganeshgonela@gmail.com'),
---     ('Pavan Kumar Mandalapalli',20,'pavankumar@gmail.com'),
---     ('Vijay Kumar Konatham',22,'vijaykumar@gmail.com'),
---     ('Sudheer Kodelli',25,'sudheerkodelli@gmail.com'),
---     ('Varun Chede',50,'varunchede@gmail.com');
+INSERT INTO USERS(name,age,email) VALUES
+    ('Durga Prasad Kota',20,'durgaprasad04289@gmail.com'),
+    ('Sidharadha Thutika',18,'siddardhathutika@gmail.com'),
+    ('Jayanth Macharla',21,'jayanth@gmail.com'),
+    ('Ganesh Gonela',20,'ganeshgonela@gmail.com'),
+    ('Pavan Kumar Mandalapalli',20,'pavankumar@gmail.com'),
+    ('Vijay Kumar Konatham',22,'vijaykumar@gmail.com'),
+    ('Sudheer Kodelli',25,'sudheerkodelli@gmail.com'),
+    ('Varun Chede',50,'varunchede@gmail.com');
 
 SELECT * FROM USERS;
 
